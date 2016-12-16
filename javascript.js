e6e489d203e87015977e37e37ae71cab2004e1a5
Andy Knolla <andy@techtonicgroup.com>
2016-12-15 22:52:42 -0700

    establish and access simple data

--- javascript.js
+++ javascript.js
@@ -1,1 +1,61 @@
 console.log("hello world");
+
+
+var library = {
+  books: [
+    {
+      title: 'Eloquent Javascript',
+      author: 'Marjin Haverbeke',
+      numberOfPages: 300,
+      publishDate: 5
+    },
+    {
+      title: 'Javascript the Good Parts',
+      author: 'Douglas Crockford',
+      numberOfPages: 200,
+      publishDate: 4
+    },
+    {
+      title: 'HTML and CSS',
+      author: 'Marjin Haverbeke',
+      numberOfPages: 500,
+      publishDate: 6
+    }
+  ],
+  addBook: function(book) {
+    //push object into library['book'] array
+  },
+
+  removerBookByTitle: function(title) {
+
+  },
+
+  removerBookByAuthor: function(author) {
+
+  },
+
+  getRandomBook: function(title) {
+
+  },
+
+  getBookByTitle: function(title) {
+
+  },
+
+  getBooksByAuthor: function (authorName) {
+
+  },
+
+  addBooks: function(books) {
+
+  },
+
+  getAuthors: function() {
+
+  },
+
+  getRandomAuthorName: function() {
+
+  }
+
+}
